12ba9505f90510a68a03a4799002d97bc5265c4f
Dinh Anh <dinhanhx@gmail.com>
2020-07-29 19:13:23 +0700

    Enforce double quote and Add comments

--- ussr.py
+++ ussr.py
@@ -5,6 +5,7 @@ from colorama import init, Fore
 init(autoreset=True)
 
 def print_help():
+    # Print cmds and descriptions
     print(Fore.YELLOW+"Welcome to Ultimate simple screen recorder")
     print(Fore.RED+"For the art, for the people, for the minimalism")
     print("Here are available cmds\n")
@@ -23,14 +24,16 @@ def print_help():
     return None
 
 def print_windows_help():
+    # Print easter egg
     print(Fore.YELLOW+"Union of Soviet Socialist Republics")
     print(Fore.RED+"Workers of the world, unite!")
     return None
 
 def setup_settings(file_name):
+    # Check, copy file to current_settings.json
     if os.path.exists(file_name):
-        fobj_r = open(file_name, 'r')
-        fobj_w = open('current_settings.json', 'w')
+        fobj_r = open(file_name, "r")
+        fobj_w = open("current_settings.json", "w")
         json.dump(json.load(fobj_r), fobj_w)
     else:
         print(Fore.RED+"Input file does not exists")
@@ -38,7 +41,8 @@ def setup_settings(file_name):
     return None
 
 def print_current_settings():
-    fobj_r = open('current_settings.json', 'r')
+    # Read, print nicely current_settings.json
+    fobj_r = open("current_settings.json", "r")
     settings = json.load(fobj_r)
     print(Fore.GREEN+"Current settings: ")
     print(json.dumps(settings, indent = 3))
@@ -49,19 +53,22 @@ if len(sys.argv) == 1:
     print_help()
 
 if len(sys.argv) >= 2:
-    if sys.argv[1] == "-h":
+    if sys.argv[1] == "-h": # ussr -h
         print_help()
 
-    if sys.argv[1] == "/?":
+    if sys.argv[1] == "/?": # ussr /?
         print_windows_help()
 
     if len(sys.argv) >= 3:
         if sys.argv[1] == "-s":
-            if sys.argv[2].split(".")[-1] == "json":
+            if sys.argv[2].split(".")[-1] == "json": # ussr -s settings.json
                 setup_settings(sys.argv[2])
 
-            if sys.argv[2] == "-i":
+            if sys.argv[2] == "-i": # ussr -s -i
                 print_current_settings()
+
+        if sys.argv[1] == "-r": # ussr -r 
+            pass
 
         else:
             print_help()
