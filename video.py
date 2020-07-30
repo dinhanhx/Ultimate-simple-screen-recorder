d8fd179e89e6fba18ac547d5845e572327a1b4b7
Dinh Anh <dinhanhx@gmail.com>
2020-07-30 10:20:57 +0700

    Add Windows handler for grab()

--- video.py
+++ video.py
@@ -1,8 +1,8 @@
 import os
 import json
 
+from platform import system
 from screeninfo import get_monitors
-from pyscreenshot import grab
 from numpy import array
 from cv2 import cvtColor, COLOR_BGR2RGB, VideoWriter_fourcc, VideoWriter
 from datetime import datetime
@@ -11,6 +11,11 @@ from keyboard import is_pressed
 from setting import load_settings
 from colorama import init, Fore
 init(autoreset=True)
+
+if system() == "Windows":
+    from PIL.ImageGrab import grab
+else:
+    from pyscreenshot import grab
 
 def get_primary_monitor():
     return (get_monitors()[0].width, get_monitors()[0].height)
@@ -28,6 +33,7 @@ def make_video():
 
     fname_path = settings["output folder"] + "/" + name_video(settings["date format"]) + ".avi"
     print(Fore.GREEN+fname_path)
+    
     vobj = VideoWriter(fname_path, VideoWriter_fourcc(*settings["fourcc"]),
                             float(settings["fps"]), get_primary_monitor())
 
