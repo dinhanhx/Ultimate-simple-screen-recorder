eac397b03ebd73355c4861f54cd441b8a4fae1d2
Dinh Anh <dinhanhx@gmail.com>
2020-07-30 16:23:22 +0700

    Switch from pyscreenshot to mss

--- video.py
+++ video.py
@@ -1,4 +1,5 @@
 import os
+import mss
 import json
 
 from screeninfo import get_monitors
@@ -11,17 +12,11 @@ from setting import load_settings
 from colorama import init, Fore
 init(autoreset=True)
 
-from platform import system
-if system() == "Windows":
-    from PIL.ImageGrab import grab
-else:
-    from pyscreenshot import grab
-
 def get_primary_monitor():
     return (get_monitors()[0].width, get_monitors()[0].height)
 
-def grab_to_opencv():
-    return cvtColor(array(grab()), COLOR_BGR2RGB)
+def grab_to_opencv(monitor):
+    return array(mss.mss().grab(monitor))[:, :, :3]
 
 def name_video(date_format):
     return datetime.now().strftime(date_format)
@@ -31,14 +26,18 @@ def make_video():
     if not os.path.exists(settings["output folder"]):
         os.mkdir(settings["output folder"])
 
-    fname_path = settings["output folder"] + "/" + name_video(settings["date format"]) + settings["tail"]
-    print(Fore.GREEN+fname_path)
+    fname_path = settings["output folder"] + "/" + \
+                name_video(settings["date format"]) + settings["tail"]
+
+    monitor = {"top": 0, "left": 0, "width": get_primary_monitor()[0],
+                                    "height": get_primary_monitor()[1]}
 
     vobj = VideoWriter(fname_path, VideoWriter_fourcc(*settings["fourcc"]),
                             float(settings["fps"]), get_primary_monitor())
 
+    print(Fore.GREEN+fname_path+Fore.White+" is recording...")
     while True:
-        vobj.write(grab_to_opencv())
+        vobj.write(grab_to_opencv(monitor))
         if is_pressed(settings["stop key"]):
             break
 
