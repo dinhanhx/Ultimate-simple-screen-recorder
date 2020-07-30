edcc23a1278a1f8d44e5ce5f2df9b3a2b90ce92e
Dinh Anh <dinhanhx@gmail.com>
2020-07-30 17:07:26 +0700

    Add make_mic(), make_speaker(), feature fs

--- audio.py
+++ audio.py
@@ -1,2 +1,15 @@
-def make_audio():
+from setting import load_settings
+
+def make_mic(cps, stop_key):
     pass
+
+def make_speaker(cps, stop_key):
+    pass
+
+def make_audio():
+    setting = load_settings()
+    if setting["mic"]:
+        make_mic(setting["fs"], setting["stop key"])
+
+    if setting["speaker"]:
+        make_speaker(setting["fs"], setting["stop key"])
